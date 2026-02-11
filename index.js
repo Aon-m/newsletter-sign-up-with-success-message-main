b20b8dad9e1d346825fc076b404931a04537cfd3
Aon <aeolaojo@gmail.com>
2026-02-11 14:18:09 +0100

    feat: implement form validation
    fix:
    - improve dialog layout
    - enable user when error message is shown

---
+++ index.js
@@ -0,0 +1,54 @@
+const closeDialogBtn = document.querySelector("#close-dialog-btn");
+const submitBtn = document.querySelector("#submit-btn");
+const successDialog = document.querySelector("#success-dialog");
+const form = document.querySelector("#form");
+const errorMessage = document.querySelector("#error-message");
+const errorBackground = document.querySelector("#error-background");
+const errorInput = document.querySelector("#email");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  emailValidation(formData);
+});
+
+errorInput.addEventListener("click", removeErrorBackground);
+
+closeDialogBtn.addEventListener("click", () => {
+  successDialog.close();
+});
+
+function emailValidation(element) {
+  let email = element.get("email");
+
+  switch (true) {
+    case email.trim() === "":
+      error();
+      return;
+    case !email.includes("@"):
+      error();
+      return;
+    case email.trim() !== "" && email.includes("@"):
+      clearError();
+      successDialog.showModal();
+      errorInput.value = "";
+  }
+}
+
+function error() {
+  errorMessage.classList.remove("hidden");
+  errorBackground.classList.add("error");
+  errorInput.classList.add("error__input");
+}
+
+function clearError() {
+  errorMessage.classList.add("hidden");
+  errorBackground.classList.remove("error");
+  errorInput.classList.remove("error__input");
+}
+
+function removeErrorBackground() {
+  errorBackground.classList.remove("error");
+}
